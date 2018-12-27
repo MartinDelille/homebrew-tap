68201f2a17cbe8e3a02d1eed69d2620089216f6f
Martin Delille <martin@phonations.com>
2018-12-27 16:49:21 +0100

    add homepage

--- pouet.rb
+++ pouet.rb
@@ -1,6 +1,6 @@
 class Pouet < Formula
   desc "A basic command line application written with Qt"
-  homepage ""
+  homepage "https://github.com/MartinDelille/pouet"
   url "https://github.com/MartinDelille/pouet/archive/0.2.tar.gz"
   sha256 "ffdb029ada6d03fe3fa323577693cc233ef3d2300784a8d79077cecf6df7a470"
   depends_on "qt" => :build
