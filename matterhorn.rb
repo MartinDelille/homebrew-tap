d7fc2e29872f329222dd1ee92fafab04a3aa87c4
Martin Delille <martin@phonations.com>
2019-02-06 14:21:42 +0100

    build matterhorn from source

--- matterhorn.rb
+++ matterhorn.rb
@@ -1,11 +1,16 @@
 class Matterhorn < Formula
   desc "Unix terminal client for the Mattermost chat system"
   homepage "https://github.com/matterhorn-chat/matterhorn"
-  url "https://github.com/matterhorn-chat/matterhorn/releases/download/50200.1.1/matterhorn-50200.1.1-Darwin-x86_64.tar.bz2"
-  sha256 "3ae6a19ecd47eeddb303970e77d5fd5379a64eda3c9b430ec80c6fd6a6429705"
+  url "https://github.com/matterhorn-chat/matterhorn.git", :tag => "50200.1.1"
+
+  depends_on "cabal-install" => :build
+  depends_on "ghc" => :build
 
   def install
-    bin.install "matterhorn"
+    system "cabal", "new-update"
+    system "cabal", "install", "tasty-quickcheck"
+    system "./build.sh"
+    bin.install "dist-newstyle/build/x86_64-osx/ghc-8.4.4/matterhorn-50200.1.1/x/matterhorn/build/matterhorn/matterhorn"
   end
 
   test do
